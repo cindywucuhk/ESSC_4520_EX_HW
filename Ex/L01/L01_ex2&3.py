e1ad3f6ef91515b32d56eefecbf921fc25d0ca67
cindywucuhk <77272480+cindywucuhk@users.noreply.github.com>
2022-01-12 08:07:34 +0000

    L01_Ex

--- Ex/L01/L01_ex2&3.py
+++ Ex/L01/L01_ex2&3.py
@@ -1,14 +1,7 @@
 ##################################################
-### Lecture 01 Exercise
+### Lecture 01 Exercise 2,3
 ### Name: Wu Hei Tung
 ### SID: 1155109536
-##################################################
-## Exercise 1
-## Print words using Python
-##################################################
-# print with the script mode
-print('Mong Man Wai Building')
-
 ##################################################
 ## Exercise 2
 ## Assign variables and print my birthday
@@ -51,7 +44,7 @@ saturated_vP = 6.1094 * e ** ((17.625 * air_T) / (air_T + 243.04))
 # RH
 RH = actual_vP / saturated_vP
 
-print(RH*100, '%')
+print(RH * 100, '%')
 
 ##################################################
 ## END
