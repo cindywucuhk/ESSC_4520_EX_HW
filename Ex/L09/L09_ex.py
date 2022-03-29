57b8de76b440fefe18118a71eee943d296c10781
cindywucuhk <77272480+cindywucuhk@users.noreply.github.com>
2022-03-29 14:18:17 +0000

    L09.ex

--- Ex/L09/L09_ex.py
+++ Ex/L09/L09_ex.py
@@ -67,14 +67,15 @@ title = 'Time series '
 # plot figure
 fig = plt.figure()
 ax = fig.add_subplot(111)
+# plot
 ax.plot(mylist, xp, label='X')
 ax.plot(mylist, yp, label='Y')
 ax.plot(mylist, zp, label='Z')
-ax.legend()
-ax.set_xlabel('timestep t')
-ax.set_title(title, fontsize=15)
+ax.legend()                         # set legend
+ax.set_xlabel('timestep t')         # set xlabel
+ax.set_title(title, fontsize=15)    # set title
 # save figure
-fig.savefig((parent_dir+save_name), dpi=300)
+fig.savefig((save_name), dpi=300)
 
 
 ##################################################
@@ -140,9 +141,9 @@ ax = fig.add_subplot(111)
 ax.plot(mylist, xi, label='(X0, Y0, Z0) = (10, 0, 0)')
 ax.plot(mylist, xii, label='(X0, Y0, Z0) = (10+0.001, 0, 0)')
 ax.plot(mylist, xiii, label='(X0, Y0, Z0) = (10-0.001, 0, 0)')
-ax.legend()
-ax.set_xlabel('timestep t')
-ax.set_title(title, fontsize=15)
+ax.legend()                         # set legend    
+ax.set_xlabel('timestep t')         # set xlabel    
+ax.set_title(title, fontsize=15)    # set title
 # save figure
 fig.savefig((parent_dir+save_name), dpi=300)
 
@@ -196,10 +197,12 @@ def Lorenz_model_M(X0, Y0, Z0, F):
   return xp, yp, zp, thist
 
 # call fns
+# experiment set 1 F=0
 xia, yia, zia, thistia = Lorenz_model_M(10.0, 0, 0, 0)
 xiia, yiia, ziia, thistiia = Lorenz_model_M(10+0.001, 0, 0, 0)
 xiiia, yiiia, ziiia, thistiiia = Lorenz_model_M(10-0.001, 0, 0, 0)
 
+# experiment set 2 F=10
 xib, yib, zib, thistib = Lorenz_model_M(10.0, 0, 0, 10)
 xiib, yiib, ziib, thistiib = Lorenz_model_M(10+0.001, 0, 0, 10)
 xiiib, yiiib, ziiib, thistiiib = Lorenz_model_M(10-0.001, 0, 0, 10)
@@ -207,32 +210,32 @@ xiiib, yiiib, ziiib, thistiiib = Lorenz_model_M(10-0.001, 0, 0, 10)
 # set save name
 save_name = 'L09_ex2c_1.png'
 # title name
-title = 'Time series of X (experiment 1)'
+title = 'Time series of X (F = 0)'
 # plot figure
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(mylist, xia, label='(X0, Y0, Z0) = (10, 0, 0)')
 ax.plot(mylist, xiia, label='(X0, Y0, Z0) = (10+0.001, 0, 0)')
 ax.plot(mylist, xiiia, label='(X0, Y0, Z0) = (10-0.001, 0, 0)')
-ax.legend()
-ax.set_xlabel('timestep t')
-ax.set_title(title, fontsize=15)
+ax.legend()                         # set legend    
+ax.set_xlabel('timestep t')         # set xlabel
+ax.set_title(title, fontsize=15)    # set title
 # save figure
 fig.savefig((parent_dir+save_name), dpi=300)
 
 # set save name
 save_name = 'L09_ex2c_2.png'
 # title name
-title = 'Time series of X (experiment 2)'
+title = 'Time series of X (F = 10)'
 # plot figure
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(mylist, xib, label='(X0, Y0, Z0) = (10, 0, 0)')
 ax.plot(mylist, xiib, label='(X0, Y0, Z0) = (10+0.001, 0, 0)')
 ax.plot(mylist, xiiib, label='(X0, Y0, Z0) = (10-0.001, 0, 0)')
-ax.legend()
-ax.set_xlabel('timestep t')
-ax.set_title(title, fontsize=15)
+ax.legend()                         # set legend
+ax.set_xlabel('timestep t')         # set xlabel
+ax.set_title(title, fontsize=15)    # title
 # save figure
 fig.savefig((parent_dir+save_name), dpi=300)
 
@@ -242,7 +245,7 @@ fig.savefig((parent_dir+save_name), dpi=300)
 ## Use Adam-Bashforth Scheme
 ##################################################
 def Lorenz_model_A(X0, Y0, Z0):
-  """ This model output the Lorenz model with setted initial condition.
+  """ This model output the Lorenz model with setted initial condition. Using third-order Adams-Bashforth scheme.
     Arguments:
       X0 - initial X
       Y0 - initial Y
@@ -253,6 +256,7 @@ def Lorenz_model_A(X0, Y0, Z0):
       zp - array of Z
       thist - array of time step
   """
+  # set list V storing X, Y and Z
   V = [[X0], [Y0], [Z0]]
   # constants
   r, sig, b = 28., 10., 8/3.
@@ -272,33 +276,42 @@ def Lorenz_model_A(X0, Y0, Z0):
     # get rid of precision issue of arange
     t = int(t*100)/100.
 
+    # third-order Adams-Bashforth scheme
+    # set X, Y, Z and dXdt, dYdt, dZdt in n=0, n=1, n=2
     if n <= 2:
+      # dXdt, dYdt, dZdt in n=0, n=1, n=2
       dXdt = sig*(V[1][n] - V[0][n])
       dYdt = r*V[0][n] - V[1][n] - V[0][n]*V[2][n]
       dZdt = V[0][n]*V[1][n] - b*V[2][n]
-      V[0].append(V[0][n] + (dXdt * dt))
-      V[1].append(V[1][n] + (dYdt * dt))
-      V[2].append(V[2][n] + (dZdt * dt))
+      # set X, Y, Z in n=0, n=1, n=2
+      V[0].append(V[0][n] + (dXdt * dt))    # X
+      V[1].append(V[1][n] + (dYdt * dt))    # Y
+      V[2].append(V[2][n] + (dZdt * dt))    # Z
+      # Store the result in xp, yp, zp
       xp[n],yp[n],zp[n] = V[0][n], V[1][n], V[2][n]
     else:
+      # create 3 list to store the dXdt, dYdt, dZdt in n>2  
       dXdt = []
       dYdt = []
       dZdt = []
+      # calculate Gn, Gn-1, Gn-2
       for i in range(0,3):
         dXdt.append(sig*(V[1][i] - V[0][i]))
         dYdt.append(r*V[0][i] - V[1][i] - V[0][i]*V[2][i])
         dZdt.append(V[0][i]*V[1][i] - b*V[2][i])
-
+      # update X, Y and Z in n-2, n-1
       for i in range(0,2):
           V[0][i] = V[0][i+1]
           V[1][i] = V[1][i+1]
           V[2][i] = V[2][i+1]
-
+      # calculate new X, Y, Z in n
       V[0][2] = V[0][2] + (dt/12) * (23*dXdt[2] - 16*dXdt[1] + 5*dXdt[0])
       V[1][2] = V[1][2] + (dt/12) * (23*dYdt[2] - 16*dYdt[1] + 5*dYdt[0])
       V[2][2] = V[2][2] + (dt/12) * (23*dZdt[2] - 16*dZdt[1] + 5*dZdt[0])
-      
+      # store the result in xp, yp, zp
       xp[n],yp[n],zp[n] = V[0][2], V[1][2], V[2][2]
+
+    # store timesteps array
     thist[n] = t
     n += 1
 
@@ -319,13 +332,31 @@ ax = fig.add_subplot(111)
 ax.plot(mylist, xi, label='(X0, Y0, Z0) = (10, 0, 0)')
 ax.plot(mylist, xii, label='(X0, Y0, Z0) = (10+0.001, 0, 0)')
 ax.plot(mylist, xiii, label='(X0, Y0, Z0) = (10-0.001, 0, 0)')
-ax.legend()
-ax.set_xlabel('timestep t')
-ax.set_title(title, fontsize=15)
+ax.legend()                         # set legend
+ax.set_xlabel('timestep t')         # set xlabel
+ax.set_title(title, fontsize=15)    # set title
 # save figure
 fig.savefig((parent_dir+save_name), dpi=300)
 
+
+
+##################################################
+## L09 Ex2e
+## Trajectory of Lorenz Model and sensitivity to 
+## initial condition
+##################################################
+# plot 3d trajectory of Lorenz Model in xyz plane
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.plot(xi, yi, zi, lw=0.5)
-fig.savefig(parent_dir + 'L09_ex2d_3d.png')
+ax.plot(xia, yia, zia, lw=0.5, label='(X0, Y0, Z0) = (10, 0, 0)')
+ax.plot(xiia, yiia, ziia, lw=0.5, label='(X0, Y0, Z0) = (10+0.001, 0, 0)')
+ax.plot(xiiia, yiiia, ziiia, lw=0.5, label='(X0, Y0, Z0) = (10-0.001, 0, 0)')
+ax.legend()                                         # set legend
+ax.set_title('Trajectory of the Lorenz Model')      # set title
+# save figure
+fig.savefig(parent_dir + 'L09_ex2e.png')
+
+
+##################################################
+## The END
+##################################################
