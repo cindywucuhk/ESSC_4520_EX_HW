121352dc6bbefa88b6a79c8c7ea8c567b8342534
cindywucuhk <77272480+cindywucuhk@users.noreply.github.com>
2022-04-26 14:22:02 +0000

    L10,11.ex

---
+++ Ex/L10/L10_ex_corr.py
@@ -0,0 +1,180 @@
+#################################################
+### Lecture 10 Exercise 1
+### Name: Wu Hei Tung
+### SID: 1155109536
+#################################################
+## Importing
+#################################################
+# import useful package
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+#################################################
+## Setting
+#################################################
+# set initial concentration
+con_O30 = 0
+con_O0 = 0
+con_NO0 = 10**12
+con_NO20 = 10**10
+
+# set variables
+h = 10          # Time step (s)
+T = 298         # Temperature (K)
+
+# set coefficients
+k1 = (1.4e3)*math.exp(1175/T)
+k2 = (1.8e-12)*math.exp(-1370/T)
+J = 1.7e-2
+
+# set list to store data
+con_O3 = [con_O30]
+con_NO2 = [con_NO20]
+con_NO = [con_NO0]
+con_O = [con_O0]
+
+#################################################
+## Solve chemical ODEs using MIE algorithm
+#################################################
+# Functions
+# 1) Backward Euler Concentration
+def Back_Euler(m_con, P_B_m, V_B_m, h):
+    """ Backward Euler Concentration
+    Arguments:
+    m_con - Concentration of the target species at iteration m (N[i,B,m])
+    P_B_m - Production rate of the target species (BE)
+    V_B_m - Loss rate/m1_con of the target species (BE)
+    h - timestep
+    Returns:
+    m1_con - Concentration of the target species at iteration m+1 (N[i,B,m+1])
+    """
+
+    m1_con = (m_con + h*P_B_m)/(1+h*V_B_m)
+
+    return m1_con
+
+# 2) Foreward Euler Concentration
+def Fore_Euler(m_con, P_B_m, L_B_m, h):
+    """ Backward Euler Concentration
+    Arguments:
+    m_con - Concentration of the target species at iteration m (N[i,F,m])
+    P_B_m - Production rate of the target species (BE)
+    L_B_m - Loss rate of the target species (BE)
+    h - timestep
+    Returns:
+    m1_con - Concentration of the target species at iteration m+1 (N[i,B,m+1])
+    """
+
+    m1_con = m_con + h*(P_B_m - L_B_m)
+
+    return m1_con
+
+# looping to find equilibrium state
+# set n = 0
+n=0
+# set first iteration
+# for Backward Euler
+con_O_B_m = con_O0
+con_NO2_B_m = con_NO20
+con_NO_B_m = con_NO0
+con_O3_B_m = con_O30
+# for Foreward Euler
+con_O_F_m = con_O0
+con_NO2_F_m = con_NO20
+con_NO_F_m = con_NO0
+con_O3_F_m = con_O30
+
+for n in range(0,200):
+    # Consecutive iterations
+    Np = 0
+    while Np < 30:
+        # calculate production rate for Backward Euler
+        P_O3_B_m = k1*con_O_B_m
+        P_NO2_B_m = k2*con_NO_B_m*con_O3_B_m
+        P_NO_B_m = J*con_NO2_B_m
+        P_O_B_m = J*con_NO2_B_m
+
+        # calculate loss rate/ m_con for Backward Euler
+        V_O3_B_m = k2*con_NO_B_m
+        V_NO2_B_m = J
+        V_NO_B_m = k2*con_O3_B_m
+        V_O_B_m = k1
+
+        # calculate Backward Euler
+        con_O3_B_m1 = Back_Euler(con_O30, P_O3_B_m, V_O3_B_m, h)
+        con_NO2_B_m1 = Back_Euler(con_NO20, P_NO2_B_m, V_NO2_B_m, h)
+        con_NO_B_m1 = Back_Euler(con_NO0, P_NO_B_m, V_NO_B_m, h)
+        con_O_B_m1 = Back_Euler(con_O0, P_O_B_m, V_O_B_m, h)
+
+        # calculate loss rate
+        L_O3_B_m = V_O3_B_m*con_O3_B_m1
+        L_NO2_B_m = V_NO2_B_m*con_NO2_B_m1
+        L_NO_B_m = V_NO_B_m*con_NO_B_m1
+        L_O_B_m = V_O_B_m*con_O_B_m1
+
+        # calculate Forward Euler
+        con_O3_F_m1 = Fore_Euler(con_O30, P_O3_B_m, L_O3_B_m, h)
+        con_NO2_F_m1 = Fore_Euler(con_NO20, P_NO2_B_m, L_NO2_B_m, h)
+        con_NO_F_m1 = Fore_Euler(con_NO0, P_NO_B_m, L_NO_B_m, h)
+        con_O_F_m1 = Fore_Euler(con_O0, P_O_B_m, L_O_B_m, h)
+
+        if n==0:
+            print(con_O3_F_m1)
+
+        # test converging
+        if (con_O3_F_m1 < 0) | (con_NO2_F_m1 < 0) | (con_NO_F_m1 < 0) | (con_O_F_m1 < 0):
+            Np = 0
+        else:
+            Np = Np+1
+
+        # renew the initial concentration
+        # for Backward Euler
+        con_O_B_m = con_O_F_m1
+        con_NO2_B_m = con_NO2_F_m1
+        con_NO_B_m = con_NO_F_m1
+        con_O3_B_m = con_O3_F_m1
+        # for Foreward Euler
+        con_O_F_m = con_O_F_m1
+        con_NO2_F_m = con_NO2_F_m1
+        con_NO_F_m = con_NO_F_m1
+        con_O3_F_m = con_O3_F_m1
+
+    # for Foreward Euler
+    con_O0 = con_O_F_m1
+    con_NO20 = con_NO2_F_m1
+    con_NO0 = con_NO_F_m1
+    con_O30 = con_O3_F_m1
+    # store the value
+    con_O3.append(con_O30)
+    con_NO2.append(con_NO20)
+    con_NO.append(con_NO0)
+    con_O.append(con_O0)
+
+    
+
+
+#################################################
+### Plot the result
+#################################################
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+ax2 = ax1.twinx()
+
+# plot figure
+plt_O = ax1.plot(h*np.arange(0, len(con_O)), np.array(con_O)*1e6, color = 'black', label = 'O(3P)*1e6')
+plt_O3 = ax1.plot(h*np.arange(0, len(con_O3)), con_O3, color = 'green', label = 'O3')
+plt_NO2 = ax1.plot(h*np.arange(0, len(con_NO2)), con_NO2, color = 'blue', label = 'NO2')
+plt_NO = ax2.plot(h*np.arange(0, len(con_NO)), con_NO, color = 'red', label = 'NO')
+
+ax1.set_xlabel('time (s)')
+ax1.set_ylabel('Conc for O,O3,NO2 (molecules per cm^3)')
+ax2.set_ylabel('Conc for NO (molecules per cm^3)')
+ax1.set_title('Concentration of different chemical species')
+
+ax1.text(330, 0.15e10, 'O(3P)*1e6', color = 'black', fontsize=12)
+ax1.text(330, 0.35e10, 'O3', color = 'green', fontsize=12)
+ax1.text(330, 0.5e10, 'NO2', color = 'blue', fontsize=12)
+ax1.text(330, 0.93e10, 'NO', color = 'red', fontsize=12)
+
+fig.savefig('L10_ex.png')
---
+++ Ex/L11/L11_ex2.py
@@ -0,0 +1,148 @@
+##################################################
+## L11 Exercise 2
+## Name: Wu Hei Tung
+## SID: 1155109536
+##################################################
+## import needed package
+##################################################
+import numpy as np
+import scipy.linalg as la
+import matplotlib.pyplot as plt
+
+#################################################
+## a) randomly assign masses between 0-1kg as 
+##    mass_true
+#################################################
+m_true = np.array(sorted(np.random.random_sample((100, 1))))
+#m_true = np.random.random_sample((100, 1))
+print(m_true)
+
+#################################################
+## b) create a correct kernel G
+#################################################
+# set N and M
+N = 100
+M = 100
+
+# create G
+G = np.zeros((N,M))
+
+# assign value for G
+for i in range(0,N):
+    # the first 2 weight
+    if i==0:
+        G[i,0]=1
+    elif i==1:
+        G[i,0]=1
+        G[i,1]=1
+    else:
+        # other case
+        G[i,i-2]=1
+        G[i,i-1]=1
+        G[i,i]=1
+
+print(G)
+
+#################################################
+## c) creates synthetic observed data d
+#################################################
+# create vector of Gaussian random numbers with zero mean and sigma is 0.01 kg
+mu = 0          # mean
+sigma_d = 0.01    # sigma
+n = np.random.normal(mu, sigma_d, size=(100,1))
+print(n)
+
+# create d
+d = np.matmul(G,m_true) + n
+print(d)
+
+#################################################
+## d) solves the inverse problem by 
+##    simple least squares
+#################################################
+# Estimated model parameters
+Gt = np.transpose(G)
+t1 = np.matmul(Gt, G)
+t2 = np.matmul(Gt, d)
+m_est = np.matmul(la.inv(t1),t2)
+dpre = np.matmul(G,m_est)
+print("prediction")
+print(dpre)
+
+#################################################
+## e) plot m_true and m_est
+#################################################
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+# plot m_true and m est
+ax.plot(m_true, color='black', label='$m^{true}$')
+ax.plot(m_est, color='red', label='$m^{est}$')
+# set legend, xlabel, ylabel, title
+ax.legend()
+ax.set_ylabel('mass (kg)')
+ax.set_xlabel('object')
+ax.set_title('True mass and least square mass of 100 objects')
+
+# save figures
+fig.savefig('L11_ex2e.png', dpi=300)
+
+#################################################
+## f) Counts up the number of estimated model 
+##    parameters that are within +-2sigma_m of 
+##    their true value
+#################################################
+# calculate sigma_m
+# variance of model parameters
+Cm = (sigma_d**2)*la.inv(t1)   
+sigma_m = []
+for i in range(M):
+    # sqrt variance of model parameter
+    sigma_m.append(np.sqrt(Cm[i,i])) 
+
+print(sigma_m)
+
+# counts up the number
+count_up = np.zeros((100,4))
+for i in range(0,M):
+    if (m_est[i][0] >= m_true[i][0]-2*sigma_m[i]) & (m_est[i][0] <= m_true[i][0]+2*sigma_m[i]):
+        count_up[i,0]=1
+    else:
+        count_up[i,0]=0
+
+    count_up[i,1]=m_est[i][0]
+    count_up[i,2]=mu-2*sigma_m[i]
+    count_up[i,3]=mu+2*sigma_m[i]
+
+total_c = np.sum(count_up[:,0])
+
+print(count_up)
+print("The number of estimated model parameters that are within +- 2 sigma_m of the true value:")
+print(total_c)
+
+#################################################
+## g) plot histogram of m_est and m_true
+#################################################
+# set the size of bin
+bin_size = 7
+bins_list = np.arange(0,1,1/bin_size)
+print(m_true.T)
+
+# plot histogram
+figh = plt.figure()
+axh = figh.add_subplot(1,1,1)
+axh.hist([m_true.T[0],m_est.T[0]],bins_list, rwidth = 0.8,label=['$m^{true}$', '$m^{est}$'])
+
+# set legend, xlabel, ylabel, title
+axh.legend()
+axh.set_xlabel('mass (kg)')
+axh.set_ylabel('number of object')
+axh.set_title('Histogram of $m^{true}$ and $m^{est}$')
+
+# save plot
+figh.savefig('L11_ex2g.png', dpi=300)
+
+
+#################################################
+### END
+#################################################
